16d77ae6608251b9f1b1ee9d9c4557f13e71b839
Neiro Mendez <alne1990@hotmail.com>
2018-05-27 12:16:51 -0400

    obteniendo los valores del centro
    agregando un metodo de busqueda y separar los centros que se repiten para la salida

--- invasion alienigena.js
+++ invasion alienigena.js
@@ -11,18 +11,19 @@ for (let i = 0; i < array.length; i++) {
         arr.push(j);
 }
 
-//console.log(array);
-//console.log(arr[0], arr);
 let pos = 1;
+
 for (let i = 0; i < arr[0]; +i++) {
     let x = getX(arr, pos);
     let y = getY(arr, pos + 1);
     let e = getEscala(arr, pos + 2);
-
-    if (arr[0] > 1 )
+    let centro = getCentro(arr, pos + 3);
+    let w = getSalida(centro);
+    if (arr[0] > 1)
         pos = otraNave(arr, pos + 3);
 
-    console.log("todo", x, y, e);
+    console.log("Salida", w);
+    //   console.log("todo", x, y, e, centro);
 }
 
 function getX(arreglo, pos) {
@@ -55,6 +56,30 @@ function getEscala(arreglo, pos) {
             break;
         }
     return value;
+}
+
+function getCentro(arreglo, pos) {
+    let cen = [];
+    for (let i = pos; i < arreglo.length; i++) {
+        if (Number(arreglo[i]))
+            break;
+        if ((arreglo[i].charCodeAt(0) >= 65 && arreglo[i].charCodeAt(0) <= 90) || (arreglo[i].charCodeAt(0) >= 97 && arreglo[i].charCodeAt(0) <= 122)) {
+            cen.push(arreglo[i].trim());
+        }
+    }
+    return cen;
+}
+
+function getSalida(salida) {
+    Array.prototype.unique = function (a) {
+        return function () {
+            return this.filter(a)
+        }
+    }(function (a, b, c) {
+        return c.indexOf(a, b + 1) < 0
+    });
+
+    return salida.unique();
 }
 
 function otraNave(arreglo, pos) {
